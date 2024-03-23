61d1e95436b79a050e4f4c1c770f05db7bea3666
chan182 <khch503@naver.com>
2024-03-23 19:27:59 +0900

    heechan commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { PrismaClient } from "@prisma/client";
+// import GoogleProvider from "next-auth/providers/google";
+// import NaverProvider from "next-auth/providers/naver";
+// import KakaoProvider from "next-auth/providers/kakao";
+// import nextAuth from "next-auth";
+
+// const prisma = new PrismaClient();
+
+// export const authOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   // Configure one or more authentication providers
+//   providers: [
+//     // ...add more providers here
+//     GoogleProvider({
+//       clientId: "test",
+//       clientSecret: "test",
+//     }),
+//   ],
+// };
+
+// export default nextAuth(authOptions);
